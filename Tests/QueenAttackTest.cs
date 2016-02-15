56db178fa0eb2c7f3da4bd9c93d86660d6d744d3
Nicholas and Will <student@epicodus.com>
2016-02-15 11:48:39 -0800

    add test for horizontal attacking

--- Tests/QueenAttackTest.cs
+++ Tests/QueenAttackTest.cs
@@ -14,7 +14,21 @@ namespace QueenAttackNS
       Assert.Equal(2, queen.GetY());
 
     }
+
     // Queen can attack on horizontal lines
+    [Fact]
+    public void CanAttack_Horizontally_true()
+    {
+      QueenAttack queen = new QueenAttack(8,2);
+      Assert.Equal(true, queen.CanAttack(8,3));
+    }
+
+    [Fact]
+    public void CanAttack_Vertically_true()
+    {
+      QueenAttack queen = new QueenAttack(8,2);
+      Assert.Equal(true, queen.CanAttack(7,2));
+    }
     // queen can attack on vertical lines
     // queen can attack diagonally
     // queen can take letter coordinants
